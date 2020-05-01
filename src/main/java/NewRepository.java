b445207f5659a90bf766e20fdc343fba03645389
mtuchkova <tuchusya@gmail.com>
2020-05-01 15:21:32 +0300

    Merge branch 'master' of C:\Users\tuchu\IdeaProjects\firstless with conflicts.

--- src/main/java/NewRepository.java
+++ src/main/java/NewRepository.java
@@ -2,11 +2,10 @@ import org.openqa.selenium.By;
 
 public class NewRepository {
     public By newrepositoryButton = By.cssSelector("[data-ga-click='Header, create new repository']");
-    public By ownerButton = By.cssSelector("[id='repository-owner']");
-    public By addgitignoreButton = By.cssSelector("[class='details-reset details-overlay select-menu']");
+    public By ownerButton = By.cssSelector("[class='form-group float-left']");
+    public By addgitignoreButton = By.cssSelector("#new_repository > div.js-with-permission-fields > div.js-repository-auto-init-options > div.d-flex.my-3 > details > summary");
     public By addlicenseButton = By.cssSelector("summary > span.text-normal");
     public By importRepositoryLink = By.cssSelector("#new_repository > div.Subhead > p > a");
-
     //public By subheadHeading = By.cssSelector("[class='Subhead-heading'][aria-label='Global'] [href='explore']");
 
 
