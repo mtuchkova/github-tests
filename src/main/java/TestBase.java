7ba8c771e2a55aacf0e9b3ff0abf6df824470889
angelinaaa <angelina.batayeva@gmail.com>
2020-05-02 19:46:13 +0300

    testng.xml

--- src/main/java/TestBase.java
+++ src/main/java/TestBase.java
@@ -1,29 +1,54 @@
-
+import Pages.HeaderTabs;
 import io.github.bonigarcia.wdm.ChromeDriverManager;
 import io.github.bonigarcia.wdm.DriverManagerType;
+import io.github.bonigarcia.wdm.OperaDriverManager;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterClass;
-
-import org.testng.annotations.BeforeClass;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.opera.OperaDriver;
+import org.testng.annotations.*;
 
 
 public class TestBase {
     WebDriver driver;
     ElementsHelper elementsHelper;
     public String testUrl = ("https://github.com/session");
+    LoginPage loginPage = new LoginPage();
+    HeaderTabs headerTabs = new HeaderTabs();
+    NewRepository newRepository = new NewRepository();
+    ImportRepository importRepository = new ImportRepository();
+    NewGist newGist = new NewGist();
+    NewOrganization newOrganization = new NewOrganization();
+    NewProject newProject = new NewProject();
 
     @BeforeClass
-    public void BeforeTests (){
-
-        ChromeDriverManager.getInstance(DriverManagerType.CHROME).setup();
-        driver = new ChromeDriver();
+    @Parameters("browser")
+    public void setUp(Browsers browser) {
+        switch (browser) {
+            case CHROME:
+                ChromeDriverManager.getInstance(DriverManagerType.CHROME).setup();
+                driver = new ChromeDriver();
+                break;
+            case FIREFOX:
+                WebDriverManager.firefoxdriver().setup();
+                driver = new FirefoxDriver();
+                break;
+            case OPERA:
+                OperaDriverManager.getInstance(DriverManagerType.OPERA).setup();
+                driver = new OperaDriver();
+                break;
+            default:
+                throw new RuntimeException("Invalid specified browser:" + browser + ",expected one of 'CHROME', 'FIREFOX', 'OPERA'");
+        }
         elementsHelper = new ElementsHelper(driver);
         driver.get(testUrl);
         driver.manage().window().maximize();
     }
+
     @AfterClass
-    public void AfterTests () {
+    public void AfterTests() {
         driver.quit();
     }
 }
+
