f61fd8bd2e2a9dfc33454255d99510e54f0731d2
angelinaaa <angelina.batayeva@gmail.com>
2020-05-04 21:16:34 +0300

    [test for plusMenuButton] first branch

--- src/main/java/ImportRepository.java
+++ src/main/java/ImportRepository.java
@@ -2,8 +2,8 @@ import org.openqa.selenium.By;
 
 public class ImportRepository {
     public By importRepositoryButton = By.cssSelector("[data-ga-click='Header, import a repository']");
-    public By cloneUrlField = By.cssSelector("#vcs_url");
-    public By cancelButton = By.cssSelector("#new_repository > div.form-actions.border-top.pt-4 > a");
-    public By beginImportButton = By.cssSelector("#new_repository > div.form-actions.border-top.pt-4 > button");
+    public By cloneUrlField = By.cssSelector("[id='vcs_url']");
+    public By cancelButton = By.cssSelector("[class='float-right btn btn-invisible']");
+    public By beginImportButton = By.cssSelector("[class='btn btn-primary']");
 
 }
--- src/main/java/NewOrganization.java
+++ src/main/java/NewOrganization.java
@@ -2,7 +2,7 @@ import org.openqa.selenium.By;
 
 public class NewOrganization {
     public By newOrganizatoinButton = By.cssSelector("[data-ga-click='Header, create new organization']");
-    public By blockFreeButton = By.cssSelector("#js-pjax-container > div.p-responsive.mt-4.mt-md-8 > div:nth-child(2) > form > div > div > div > div:nth-child(1) > div.d-none.d-sm-block.height-full > div > div > label > div.d-flex.flex-column.flex-1.flex-justify-end.mb-2 > div.m-2 > button");
-    public By blockTeamButton = By.cssSelector("#js-pjax-container > div.p-responsive.mt-4.mt-md-8 > div:nth-child(2) > form > div > div > div > div:nth-child(2) > div.d-none.d-sm-block.height-full > div > div > label > div.d-flex.flex-column.flex-1.flex-justify-end.mb-2 > div.m-2 > button");
-    public By blockEnterpriseButton = By.cssSelector("#js-pjax-container > div.p-responsive.mt-4.mt-md-8 > div:nth-child(2) > form > div > div > div > div:nth-child(3) > div.d-none.d-sm-block.height-full > div > div > label > div.d-flex.flex-column.flex-1.flex-justify-end.mb-2 > div.m-2 > button");
+    public By blockFreeButton = By.cssSelector("[class='btn-mktg signup-btn  f4 signup-plan-card-light-text width-full']");
+    public By blockTeamButton = By.cssSelector("[data-ga-click='Signup funnel select org plan,click,text:Choose Team; meta: user_id: f7e83464b2232e2a2464c028c4185a02']");
+    public By blockEnterpriseButton = By.cssSelector("[data-ga-click='Signup funnel select org plan,click,text:Start your 14-day free trial; meta: user_id: f7e83464b2232e2a2464c028c4185a02']");
 }
