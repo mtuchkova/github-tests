bedae4b8480707fb881fce8002e663e13b81a59f
angelinaaa <angelina.batayeva@gmail.com>
2020-05-06 23:16:59 +0300

    {{plusmenu}}create packcage pages,readme.md,structure of code,locators

--- src/main/java/NewOrganization.java
+++
@@ -1,8 +0,0 @@
-import org.openqa.selenium.By;
-
-public class NewOrganization {
-    public By newOrganizatoinButton = By.cssSelector("[data-ga-click='Header, create new organization']");
-    public By blockFreeButton = By.cssSelector("[class='btn-mktg signup-btn  f4 signup-plan-card-light-text width-full']");
-    public By blockTeamButton = By.cssSelector("[data-ga-click='Signup funnel select org plan,click,text:Choose Team; meta: user_id: f7e83464b2232e2a2464c028c4185a02']");
-    public By blockEnterpriseButton = By.cssSelector("[data-ga-click='Signup funnel select org plan,click,text:Start your 14-day free trial; meta: user_id: f7e83464b2232e2a2464c028c4185a02']");
-}
