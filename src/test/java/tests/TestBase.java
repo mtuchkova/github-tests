8ee242eb2523ab9ef3841bc04bc41e7287a70238
mtuchkova <tuchusya@gmail.com>
2020-11-28 23:10:24 +0200

    cd 4

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,42 +1,32 @@
 package tests;
 
 import helpers.ElementsHelper;
-import io.github.bonigarcia.wdm.*;
-import io.github.bonigarcia.wdm.config.DriverManagerType;
-import io.github.bonigarcia.wdm.managers.ChromeDriverManager;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+
 import org.testng.annotations.*;
 
 public class TestBase {
 
-    public  static WebDriver driver;
+    public static WebDriver driver;
     public static ElementsHelper elementsHelper;
     public String testUrl = ("https://github.com/login");
 
-    @BeforeGroups(alwaysRun = true,groups = {"git_test"})
+    @BeforeGroups(alwaysRun = true, groups = {"git_test"})
     @Parameters("browser")
-    public void setUp( Browsers browser) {
-        switch (browser) {
-            case CHROME:
-                ChromeDriverManager.getInstance(DriverManagerType.CHROME).setup();
+    public void setUp(Browsers browser) {
+//                ChromeDriverManager.getInstance(DriverManagerType.CHROME).setup();
 //                ChromeDriverManager.chromedriver().browserVersion("87.0.4280.66").setup();
-                driver = new ChromeDriver();
-                break;
-            case FIREFOX:
-                 WebDriverManager.firefoxdriver().setup();
-                 driver = new FirefoxDriver();
-                 break;
-            default:
-                throw new RuntimeException("Invalid specified browser:" + browser + ",expected one of 'CHROME', 'FIREFOX', 'OPERA'");
-        }
+        System.setProperty("webdriver.chrome.driver", "C:/gitPractice/chromedriver_win32/chromedriver.exe");
+        driver = new ChromeDriver();
+
         elementsHelper = new ElementsHelper(driver);
         driver.get(testUrl);
         driver.manage().window().maximize();
     }
 
-   @AfterGroups(alwaysRun = true,groups = {"git_test"})
+    @AfterGroups(alwaysRun = true, groups = {"git_test"})
     public void AfterTests() {
 
         driver.quit();
